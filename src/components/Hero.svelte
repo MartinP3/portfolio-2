8ccdb70bb371a756d3f55f9b3e4cb66453cf4415
Martin P <70173574+MartinP3@users.noreply.github.com>
2023-06-04 14:44:39 +0200

    work in progress components

---
+++ src/components/Hero.svelte
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+<section class="heroDiv p-4 flex justify-center items-center">
+  <div class="uppercase text-7xl bg-neutral-100 p-4">Martin Pedersen</div>
+</section>
+
+<style>
+.heroDiv {
+  height: 100vh;
+  width: 100%;
+  background-color: rgb(32, 68, 56);
+  font-family: 'Poppins', sans-serif;
+}
+</style>
---
+++ src/components/Portfolio.svelte
@@ -0,0 +1,71 @@
+<script>
+  import { Card } from "flowbite-svelte";
+  import Image from "../assets/230602110916.png"
+
+  let isFlipped = false;
+
+  function toggleFlip() {
+    isFlipped = !isFlipped;
+  }
+</script>
+
+<section class="portfolioSection flex flex-col items-center my-4 gap-4">
+  <div>
+    <div class="flip-card w-96" class:flipped={isFlipped} on:click={toggleFlip} on:keydown={toggleFlip}>
+      <div class="flip-card-inner">
+        <div class="frontside">
+          <Card padding="none" class="p-4">
+            <img src={Image} alt="lol" />
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Semester Project 2</h5>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">
+              Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+            </p>
+          </Card>
+        </div>
+        <div class="backside">
+          <Card padding="none" class="p-4 backside">
+            <img src={Image} alt="lol" />
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">FLIPPED</h5>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam adipisci eius nesciunt reprehenderit. Eius ipsam
+              excepturi asperiores quibusdam exercitationem amet numquam, accusantium, nihil, officiis dicta possimus nemo
+              dolores aut facere.
+            </p>
+            <p class="text-center">Links here</p>
+          </Card>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<style>
+  .flip-card {
+    perspective: 900px;
+    cursor: pointer;
+  }
+
+  .flip-card-inner {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    transform-style: preserve-3d;
+    transition: transform 0.5s;
+  }
+
+  .flip-card.flipped .flip-card-inner {
+    transform: rotateY(180deg);
+  }
+
+  .frontside,
+  .backside {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    backface-visibility: hidden;
+  }
+
+  .backside {
+    transform: rotateY(180deg);
+  }
+</style>
