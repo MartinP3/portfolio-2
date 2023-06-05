15f995c50433c114e98bf5bf5c93b61e01d3477c
Martin P <70173574+MartinP3@users.noreply.github.com>
2023-06-05 14:46:37 +0200

    added Contact section to components and page layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,13 @@
 <script>
   import Hero from "../components/Hero.svelte";
-  import Portfolio from '../components/Portfolio.svelte';
   import AboutMe from '../components/AboutMe.svelte';
+  import Portfolio from '../components/Portfolio.svelte';
+  import Contact from '../components/Contact.svelte';
 </script>
 
 <div>
   <Hero />
   <AboutMe />
   <Portfolio />
+  <Contact />
 </div>
