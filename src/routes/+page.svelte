cafafaf0370aae00a389078c5280988362eceb94
Martin P <70173574+MartinP3@users.noreply.github.com>
2023-06-10 15:24:37 +0200

    added a page title, myskills section and scroll behavior

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,26 @@
 <script>
-  import Hero from "../components/Hero.svelte";
-  import AboutMe from '../components/AboutMe.svelte';
-  import Portfolio from '../components/Portfolio.svelte';
-  import Contact from '../components/Contact.svelte';
+  import Hero from "../components/static/Hero.svelte";
+  import AboutMe from '../components/static/AboutMe.svelte';
+  import Portfolio from '../components/static/Portfolio.svelte';
+  // @ts-ignore for some reason it doesn't like this import while I'm using font awesome  icons?
+  import Contact from '../components/static/Contact.svelte';
+  import MySkills from '../components/static/MySkills.svelte';
+  
 </script>
 
+<svelte:head>
+  <title>Martin Pedersen</title>
+</svelte:head>
 <div>
   <Hero />
   <AboutMe />
+  <MySkills />
   <Portfolio />
   <Contact />
 </div>
+
+<style>
+  * {
+    scroll-behavior: smooth;
+  }
+</style>
